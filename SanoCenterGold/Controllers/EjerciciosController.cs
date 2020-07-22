c127fc167a7f7a433edb0c9452e9f3e091465216
Serpentalis87 <serpentalis@hotmail.es>
2020-07-22 10:01:11 +0200

    cambios css,gifs y controller ejercicios para la vista retos y ejercicios explicados.

--- SanoCenterGold/Controllers/EjerciciosController.cs
+++ SanoCenterGold/Controllers/EjerciciosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,11 @@ namespace SanoCenterGold.Controllers
 
         // GET: Ejercicios
         public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ejercicio.ToListAsync());
+        }
+        [Authorize]
+         public async Task<IActionResult> ComoRealizarlos()
         {
             return View(await _context.Ejercicio.ToListAsync());
         }
